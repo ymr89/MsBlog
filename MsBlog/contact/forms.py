a0f351082a8c5a5888ce5dc50c072902a1e6e67a
Yadi Reyes <ymr89@uw.edu>
2016-08-08 12:03:18 -0700

    Added
    Website

---
+++ MsBlog/contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    contact_message = forms.CharField(required=True, widget=forms.Textarea)
+
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['contact_name'].label = "Your name:"
+        self.fields['contact_email'].label = "Your email:"
+        self.fields['contact_message'].label = "Message:"
---
+++ MsBlog/posts/templatetags/urlify.py
@@ -0,0 +1,13 @@
+try:
+    from urllib.parse import quote_plus #python 3
+except: 
+    pass
+
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def urlify(value):
+    return quote_plus(value)
